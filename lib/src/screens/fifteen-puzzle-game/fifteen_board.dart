9923a9024893a1f9c5f8e2479db8cb24c8e5eb1e
shaux <54446329+Shaurya-Sarma@users.noreply.github.com>
2020-06-15 17:51:55 -0400

    Redesigned 15 Game

--- lib/src/screens/fifteen-puzzle-game/fifteen_board.dart
+++ lib/src/screens/fifteen-puzzle-game/fifteen_board.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_timer/flutter_timer.dart';
 import 'package:tabletop_gui/src/blocs/fifteen-puzzle-game/fifteen_bloc.dart';
 import 'package:tabletop_gui/src/blocs/fifteen-puzzle-game/fifteen_bloc_provider.dart';
 import 'package:tabletop_gui/src/utils/audio_manager.dart';
@@ -13,6 +14,49 @@ class _FifteenPuzzleBoardState extends State<FifteenPuzzleBoard> {
   FifteenBloc _bloc;
   bool isTimerActive = false;
   bool isGameOver = false;
+  final Stopwatch _stopwatch = Stopwatch();
+  final duration = Duration(milliseconds: 1);
+  String timerDisplay = "00:00:00";
+  int numberOfMoves = 0;
+
+  void startStopwatch() {
+    setState(() {
+      isTimerActive = true;
+    });
+    _stopwatch.start();
+    startTimer();
+  }
+
+  void startTimer() {
+    Timer(duration, keeprunning);
+  }
+
+  void keeprunning() {
+    if (_stopwatch.isRunning) {
+      startTimer();
+    }
+    setState(() {
+      timerDisplay =
+          (_stopwatch.elapsed.inMinutes % 60).toString().padLeft(2, "0") +
+              ":" +
+              (_stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, "0") +
+              ":" +
+              (_stopwatch.elapsedMilliseconds % 60).toString().padLeft(2, "0");
+    });
+  }
+
+  void pauseStopwatch() {
+    setState(() {
+      isTimerActive = false;
+    });
+    _stopwatch.stop();
+  }
+
+  void resestGame() {
+    _stopwatch.reset();
+    timerDisplay = "00:00:00";
+    numberOfMoves = 0;
+  }
 
   void moveTile(int index) {
     if (isAdjacent(index)) {
@@ -22,8 +66,9 @@ class _FifteenPuzzleBoardState extends State<FifteenPuzzleBoard> {
         _bloc.tileNumbers[index] = 16;
         _bloc.tileNumbers[emptyTileIndex] = clickedTileNumber;
         // AudioManager.playSound("fifteen_tile_slide.mp3", 1.1);
-        checkWin();
+        numberOfMoves += 1;
       });
+      checkWin();
     }
   }
 
@@ -39,7 +84,7 @@ class _FifteenPuzzleBoardState extends State<FifteenPuzzleBoard> {
 
   void checkWin() {
     isGameOver = _bloc.tileNumbers.every((tile) {
-      if (tile == _bloc.tileNumbers.indexOf(tile) - 1) {
+      if (tile == _bloc.tileNumbers.indexOf(tile) + 1) {
         return true;
       } else {
         return false;
@@ -68,30 +113,53 @@ class _FifteenPuzzleBoardState extends State<FifteenPuzzleBoard> {
     return Scaffold(
       body: Container(
         color: Colors.brown[100],
+        padding: EdgeInsets.symmetric(
+          horizontal: 40.0,
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // timer(),
+            backArrow(),
+            timer(),
             gameBoard(),
             actionButtons(),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Text("$numberOfMoves Moves",
+                  style:
+                      TextStyle(fontWeight: FontWeight.w400, fontSize: 24.0)),
+            )
           ],
         ),
       ),
     );
   }
 
-  // Widget timer() {
-  //   return TikTikTimer(
-  //     height: 150,
-  //     width: 150,
-  //     initialDate: DateTime.now(),
-  //     running: isTimerActive,
-  //   );
-  // }
+  Widget backArrow() {
+    return Container(
+      alignment: Alignment.topLeft,
+      padding: EdgeInsets.only(bottom: 30.0),
+      child: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back,
+            size: 30.0,
+          )),
+    );
+  }
+
+  Widget timer() {
+    return Text(
+      "$timerDisplay",
+      style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.w800),
+    );
+  }
 
   Widget gameBoard() {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40.0),
+      padding: EdgeInsets.symmetric(vertical: 15.0),
       child: ClipRRect(
           borderRadius: BorderRadius.circular(10.0),
           child: Container(
@@ -103,11 +171,11 @@ class _FifteenPuzzleBoardState extends State<FifteenPuzzleBoard> {
                     crossAxisCount: FifteenBloc.tilesPerRow),
                 itemBuilder: (BuildContext context, int index) {
                   return GestureDetector(
-                    onTap: isGameOver
-                        ? null
-                        : () {
+                    onTap: isTimerActive
+                        ? () {
                             moveTile(index);
-                          },
+                          }
+                        : null,
                     child: Padding(
                       padding: EdgeInsets.all(8.0),
                       child: numberTile(index),
@@ -145,37 +213,31 @@ class _FifteenPuzzleBoardState extends State<FifteenPuzzleBoard> {
 
   Widget actionButtons() {
     return ButtonBar(
-      alignment: MainAxisAlignment.center,
+      alignment: MainAxisAlignment.spaceAround,
       children: <Widget>[
-        FlatButton(
-          child: Text(
-            "Start",
-            style: TextStyle(color: Colors.green[600]),
-          ),
-          onPressed: () {
-            setState(() {
-              isTimerActive = true;
-            });
-          },
-        ),
-        FlatButton(
-          child: Text(
-            "Stop",
-            style: TextStyle(color: Colors.red[600]),
-          ),
-          onPressed: () {
-            setState(() {
-              isTimerActive = false;
-            });
-          },
-        ),
+        IconButton(
+            icon: Icon(Icons.play_arrow),
+            splashColor: Colors.green[400],
+            iconSize: 30.0,
+            onPressed: () {
+              startStopwatch();
+            }),
+        IconButton(
+            icon: Icon(Icons.pause),
+            splashColor: Colors.red[600],
+            iconSize: 30.0,
+            onPressed: () {
+              pauseStopwatch();
+            }),
         RaisedButton(
-          child: Text("New Puzzle +", style: TextStyle(color: Colors.white)),
-          color: Colors.amber,
+          child: Text("New Puzzle", style: TextStyle(color: Colors.white)),
+          color: Colors.amber[600],
           onPressed: () {
             setState(() {
               _bloc.tileNumbers.shuffle();
             });
+            resestGame();
+            pauseStopwatch();
           },
         )
       ],
@@ -188,17 +250,20 @@ class _FifteenPuzzleBoardState extends State<FifteenPuzzleBoard> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text("You Have Solved The Puzzle!"),
-          content: Text("You solved it in _ minutes and _ seconds"),
+          content: Text(
+              "You solved it in ${_stopwatch.elapsed.inMinutes} minutes and ${_stopwatch.elapsed.inSeconds} seconds and in $numberOfMoves. "),
           actions: <Widget>[
             FlatButton(
               child: Text("Close"),
               onPressed: () {
                 Navigator.of(context).pop();
+                resestGame();
               },
             ),
           ],
         );
       },
     );
+    pauseStopwatch();
   }
 }
