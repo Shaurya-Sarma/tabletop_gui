a8e52bf34c2e7ee2078a21bd29e715a2325a4923
shaux <54446329+Shaurya-Sarma@users.noreply.github.com>
2020-07-10 00:22:31 -0400

    Redesign

--- lib/src/screens/games.dart
+++ lib/src/screens/games.dart
@@ -5,136 +5,110 @@ import 'package:tabletop_gui/src/blocs/war-card-game/war_lobby_bloc_provider.dar
 import 'package:tabletop_gui/src/screens/fifteen-puzzle-game/fifteen_board.dart';
 import 'package:tabletop_gui/src/screens/snake-game/snake-game_board.dart';
 import 'package:tabletop_gui/src/screens/war-card-game/war_lobby.dart';
-import 'package:tabletop_gui/src/utils/strings.dart';
 
-class GamesScreen extends StatelessWidget {
+class GamesScreen extends StatefulWidget {
+  @override
+  _GamesScreenState createState() => _GamesScreenState();
+}
+
+class _GamesScreenState extends State<GamesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 50.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              twentyNine(context),
-              snakeGame(context),
-              fifteenPuzzle(context)
-            ],
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 60.0, left: 30.0, bottom: 35.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "My Games",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 34.0,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                    child: _buildGameCard("arcade_icon.png", "Snake"),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SnakeGameBlocProvider(
+                                    child: SnakeGameBoard(),
+                                  )));
+                    }),
+                GestureDetector(
+                    child: _buildGameCard("cards_icon.png", "War"),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => WarLobbyBlocProvider(
+                                    child: LobbyScreen(),
+                                  )));
+                    }),
+                GestureDetector(
+                    child: _buildGameCard("puzzle_icon.png", "Fifteen"),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FifteenBlocProvider(
+                                    child: FifteenPuzzleBoard(),
+                                  )));
+                    }),
+                SizedBox(height: 60)
+              ],
+            ),
+            Positioned(
+              right: 0,
+              top: 0,
+              child: Container(
+                height: 180,
+                width: 180,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/blue_banner.png'),
+                        fit: BoxFit.cover)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGameCard(String imageName, String gameTitle) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 35.0),
+      padding: EdgeInsets.symmetric(vertical: 28.0, horizontal: 40.0),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: [
+            BoxShadow(
+                color: Color(0x64000000), blurRadius: 5.0, offset: Offset(1, 2))
+          ]),
+      child: Column(
+        children: <Widget>[
+          Image(
+              image: AssetImage("assets/images/$imageName"),
+              height: 150,
+              width: 150),
+          Container(
+            margin: EdgeInsets.only(top: 15.0),
+            child: Text(gameTitle,
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24.0)),
           ),
-        ),
+        ],
       ),
-    );
-  }
-
-  Widget twentyNine(BuildContext context) {
-    return GestureDetector(
-      child: Card(
-        color: Color(0xff212049),
-        margin: EdgeInsets.symmetric(vertical: 30.0),
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Image.asset("assets/images/war_thumbnail.jpg"),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Text(
-                    StringConstant.warTitle,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return WarLobbyBlocProvider(child: LobbyScreen());
-        }));
-      },
-    );
-  }
-
-  Widget snakeGame(BuildContext context) {
-    return GestureDetector(
-      child: Card(
-        color: Color(0xff212049),
-        margin: EdgeInsets.symmetric(vertical: 30.0),
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Image.asset("assets/images/snake_game_thumbnail.png"),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Text(
-                    StringConstant.snakeGameTitle,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return SnakeGameBlocProvider(child: SnakeGameBoard());
-        }));
-      },
-    );
-  }
-
-  Widget fifteenPuzzle(BuildContext context) {
-    return GestureDetector(
-      child: Card(
-        color: Color(0xff212049),
-        margin: EdgeInsets.symmetric(vertical: 30.0),
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Image.asset("assets/images/fifteen_puzzle_thumbnail.jpg"),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Text(
-                    StringConstant.fifteenPuzzleTitle,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return FifteenBlocProvider(child: FifteenPuzzleBoard());
-        }));
-      },
     );
   }
 }
--- lib/src/screens/profile.dart
+++ lib/src/screens/profile.dart
@@ -15,58 +15,73 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0xffF1F7FF),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: Stack(
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.fromLTRB(30.0, 55.0, 30.0, 70.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "My Profile",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 34.0,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.fromLTRB(30.0, 60.0, 30.0, 70.0),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "My Profile",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 34.0,
+                          ),
+                        ),
                       ),
-                    ),
+                      Container(
+                        child: CircleAvatar(
+                          backgroundImage:
+                              NetworkImage("${widget.user.photoUrl}"),
+                          radius: 85,
+                        ),
+                        margin: EdgeInsets.only(top: 60.0, bottom: 30.0),
+                      ),
+                      Text(
+                        widget.user.displayName,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 28.0),
+                        softWrap: true,
+                      )
+                    ],
                   ),
-                  Container(
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage("${widget.user.photoUrl}"),
-                      radius: 85,
-                    ),
-                    margin: EdgeInsets.only(top: 60.0, bottom: 30.0),
-                  ),
-                  Text(
-                    widget.user.displayName,
-                    style:
-                        TextStyle(fontWeight: FontWeight.w600, fontSize: 28.0),
-                    softWrap: true,
-                  )
-                ],
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20.0),
-                      bottomRight: Radius.circular(20.0)),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Color(0x23000000),
-                        blurRadius: 15.0,
-                        offset: Offset(0, 4))
-                  ]),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color(0x23000000), offset: Offset(0, 4))
+                      ]),
+                ),
+                Container(
+                  height: 200,
+                  margin: EdgeInsets.only(top: 50.0),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/profile_art.png"))),
+                )
+              ],
             ),
-            Container(
-              height: 200,
-              margin: EdgeInsets.only(top: 50.0),
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("assets/images/profile_art.png"))),
-            )
+            Positioned(
+              right: 0,
+              top: 0,
+              child: Container(
+                height: 180,
+                width: 180,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/green_banner.png'),
+                        fit: BoxFit.cover)),
+              ),
+            ),
           ],
         ));
   }
